38074119668fdcc3a631e3d6db9a43539a9fd925
Practicum <kasymhr@gmail.com>
2026-01-09 16:36:26 +0500

    Init commit

--- app/src/main/java/com/example/playlist_maker2/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker2/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.playlist_maker2
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +15,25 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val searchBtn = findViewById<LinearLayout>(R.id.search_btn)
+        val mediaBtn =findViewById<LinearLayout>(R.id.media_btn)
+        val settingBtn= findViewById<LinearLayout>(R.id.settings_btn)
+
+
+        searchBtn.setOnClickListener {
+            val searchIntent= Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        mediaBtn.setOnClickListener {
+            val mediaIntent= Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+
+        settingBtn.setOnClickListener {
+            val settingsIntent= Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
         }
     }
 }
