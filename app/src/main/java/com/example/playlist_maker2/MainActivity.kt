ab6775451445674efa3d21cd332d2b87ce3ec0f5
kasym0055 <78597512+kasym0055@users.noreply.github.com>
2026-02-16 13:44:51 +0500

    Revert "final hw"

--- app/src/main/java/com/example/playlist_maker2/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker2/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.playlist_maker2
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -16,7 +13,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,9 +35,5 @@ class MainActivity : AppCompatActivity() {
             val settingsIntent= Intent(this, SettingsActivity::class.java)
             startActivity(settingsIntent)
         }
-
-
-
     }
-
 }
